5231ef778b5e38d5e043542073077542d3e48d07
Ifiok Udoidiok <ifioku@wolfram.com>
2021-08-25 18:57:26 +0100

    Refactor NavBar to minor styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box, Image, Text, Button, Stack } from "@chakra-ui/react";
+import { Box, Flex, Stack } from "@chakra-ui/react";
 
-const NavBar = () => {
-  return (
-    <Box as ='nav' d='flex' flexDirection='row' justifyContent='space-between'>
-        {/** d='flex' */}
-      <Box>
-        <Link to="/">Home</Link> &nbsp;&nbsp;&nbsp;
-        <Link to="favourite">Favourite</Link>
-      </Box>
-      <Link to="/cart">
-        <Button>shopping cart</Button>
-      </Link>
-    </Box>
-  );
+const NavBar = ({ to = "/" }) => {
+ 
+    return (
+        <Flex
+        direction='row'
+          as="nav"
+          p={4}
+          bg="grey"
+          top={0}
+          w="100"
+          justify="space-between"
+        >
+         
+            <Link to="/">
+              HOME{" "}
+            </Link>
+            <Link to="/favorite">MY FAVORITE</Link>
+            <Link to="#">Search bar placeholder </Link>
+            <Link to="/cart">Cart ICON </Link>
+          
+        </Flex>
+      );
 };
 
+
 export default NavBar;
+
