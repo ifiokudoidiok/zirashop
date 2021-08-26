8e5cc7a25a53ad26a6c2505f4b73fd7b8ccffb0c
Ifiok Udoidiok <ifioku@wolfram.com>
2021-08-26 23:51:49 +0100

    Functional Search Logic Temporarily in Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addToCart, addFave, removeFave } from "./actions/cartActions";
-import { Box, Image, Text, Button, Stack } from "@chakra-ui/react";
+import { Box, Image, Text, Button, Stack, Input } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
 import { GrFavorite } from "react-icons/gr";
 import { MdFavorite, MdAddShoppingCart } from "react-icons/md";
@@ -13,7 +13,6 @@ const Home = (props: any) => {
   const handleClick = (id: any) => {
     props.addToCart(id);
   };
- 
   const addToFave = (id: any) => {
     props.addFave(id);
   };
@@ -21,6 +20,25 @@ const Home = (props: any) => {
   const removeFromFave = (id: any) => {
     props.removeFave(id);
   };
+
+
+  // Search
+
+const [searchTerm, setSearchTerm] = React.useState("");
+ const [searchResults, setSearchResults] = React.useState([] as Array<any>[]);
+  const handleChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+  React.useEffect(() => {
+    console.log(props, 'Props')
+    const results: any = props.items.filter((item: any)=>
+      item.title.toLowerCase().includes(searchTerm)
+    );
+    
+    setSearchResults(results);
+    console.log(searchTerm, searchResults);
+  }, [searchTerm]);
 
 
   let itemList = props.items.map((item: any) => {
@@ -65,8 +83,58 @@ const Home = (props: any) => {
       </Box>
     );
   });
+
+  let searchList = searchResults.map((item: any) => {
+    return (
+      <Box>   
+        <Box key={item.id}>
+          <Box>
+            <Image src={item.img} alt={item.title} boxSize="250px" />
+          </Box>
+          <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight">
+            Title: {item.title}
+          </Box>
+          <Box display="flex" justifyContent="space-around">
+            {!item.fave ? (
+              <Box
+                key={item.id}
+                cursor="pointer"
+                onClick={() => addToFave(item.id)}
+              >
+                <Icon as={GrFavorite} />
+              </Box>
+            ) : (
+              <Box
+                key={item.id}
+                cursor="pointer"
+                onClick={() => removeFromFave(item.id)}
+              >
+                <Icon as={MdFavorite} />
+              </Box>
+            )}
+
+            <Icon
+              onClick={() => {
+                handleClick(item.id);
+              }}
+              as={MdAddShoppingCart}
+              cursor="pointer"
+            />
+          </Box>
+          <br />
+        </Box>
+      </Box>
+    );
+  });
+
+
   return (
     <Box>
+      <Input
+        borderRadius={2}
+        value={searchTerm}
+        onChange={handleChange}
+      ></Input>
       <Text>Items</Text>
       <Box
         display="flex"
@@ -74,7 +142,7 @@ const Home = (props: any) => {
         padding={2}
         flexWrap="wrap"
       >
-        {itemList}
+        {searchTerm ? searchList : itemList}
       </Box>
     </Box>
   );
@@ -83,6 +151,8 @@ const Home = (props: any) => {
 const mapStateToProps = (state: any) => {
   return {
     items: state.items,
+    // searchTerm: state.searchTerm,
+    // searchResult: state.searchResult
   };
 };
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,33 +1,58 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { Box, Flex, Stack } from "@chakra-ui/react";
+import { Flex, Input } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
-import {  GrCart } from "react-icons/gr";
+import { GrCart } from "react-icons/gr";
 
-const NavBar = ({ to = "/" }) => {
+const NavBar = (props: any) => {
+  // const [searchTerm, setSearchTerm] = React.useState("");
+  // const [searchResults, setSearchResults] = React.useState([] as Array<any>[]);
+  //  const handleChange = (event: any) => {
+  //    setSearchTerm(event.target.value);
+  //  };
  
-    return (
-        <Flex
-        direction='row'
-          as="nav"
-          p={4}
-          bg="grey"
-          top={0}
-          w="100"
-          justify="space-between"
-        >
-         
-            <Link to="/">
-              HOME{" "}
-            </Link>
-            <Link to="/favorite">MY FAVORITE</Link>
-            <Link to="#">Search bar placeholder </Link>
-            <Link to="/cart" > <Icon as={GrCart} cursor='pointer'/></Link>
-          
-        </Flex>
-      );
+  //  React.useEffect(() => {
+  //    console.log(props, 'Props')
+  //    const results: any = props.items.filter((item: any)=>
+  //      item.title.toLowerCase().includes(searchTerm)
+  //    );
+     
+  //    setSearchResults(results);
+  //    console.log(searchTerm, searchResults);
+  //  }, [searchTerm]);
+
+  return (
+    <Flex
+      direction="row"
+      as="nav"
+      p={4}
+      bg="grey"
+      top={0}
+      w="100"
+      justify="space-between"
+    >
+      <Link to="/">HOME </Link>
+      <Link to="/favorite">MY FAVORITE</Link>
+      {/* <Input
+        borderRadius={2}
+        value={searchTerm}
+        onChange={handleChange}
+      ></Input> */}
+      <Link to="/cart">
+        {" "}
+        <Icon as={GrCart} cursor="pointer" />
+      </Link>
+    </Flex>
+  );
 };
 
+const mapStateToProps = (state: any) => {
+  return {
+    items: state.items,
+    // searchTerm: state.searchTerm,
+    // searchResults: state.searchResults
+  };
+};
 
-export default NavBar;
-
+export default connect(mapStateToProps)(NavBar);
