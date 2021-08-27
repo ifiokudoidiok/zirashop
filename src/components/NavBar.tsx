72c151fe31a4606190c61431683fb1552fc973dc
Ifiok Udoidiok <ifioku@wolfram.com>
2021-08-27 13:12:09 +0100

    Remove search logic from navbar and move to parent component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,10 @@
 import React from "react";
-import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Flex, Input } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
 import { GrCart } from "react-icons/gr";
 
 const NavBar = (props: any) => {
-  // const [searchTerm, setSearchTerm] = React.useState("");
-  // const [searchResults, setSearchResults] = React.useState([] as Array<any>[]);
-  //  const handleChange = (event: any) => {
-  //    setSearchTerm(event.target.value);
-  //  };
- 
-  //  React.useEffect(() => {
-  //    console.log(props, 'Props')
-  //    const results: any = props.items.filter((item: any)=>
-  //      item.title.toLowerCase().includes(searchTerm)
-  //    );
-     
-  //    setSearchResults(results);
-  //    console.log(searchTerm, searchResults);
-  //  }, [searchTerm]);
-
   return (
     <Flex
       direction="row"
@@ -34,11 +17,11 @@ const NavBar = (props: any) => {
     >
       <Link to="/">HOME </Link>
       <Link to="/favorite">MY FAVORITE</Link>
-      {/* <Input
+      <Input
         borderRadius={2}
-        value={searchTerm}
-        onChange={handleChange}
-      ></Input> */}
+        value={props.searchTerm}
+        onChange={props.handleChange}
+      ></Input>
       <Link to="/cart">
         {" "}
         <Icon as={GrCart} cursor="pointer" />
@@ -47,12 +30,4 @@ const NavBar = (props: any) => {
   );
 };
 
-const mapStateToProps = (state: any) => {
-  return {
-    items: state.items,
-    // searchTerm: state.searchTerm,
-    // searchResults: state.searchResults
-  };
-};
-
-export default connect(mapStateToProps)(NavBar);
+export default NavBar;
