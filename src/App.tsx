b1498813d0fef58f88405e65c1c26f09e5042eac
Ifiok Udoidiok <ifioku@wolfram.com>
2021-08-27 13:10:34 +0100

    Connect parent to redux for search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import Home from './components/Home';
-import Cart from './components/Cart';
+import Home from "./components/Home";
+import Cart from "./components/Cart";
 import "./App.css";
 import Fave from "./components/Fave";
 
-function App() {
+function App(props: any) {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([] as Array<any>[]);
+  const handleChange = (event: any) => {
+    console.log(event.target.value);
+    setSearchTerm(event.target.value);
+  };
+
+  React.useEffect(() => {
+    console.log(props, "Props");
+    const results: any = props.items.filter((item: any) =>
+      item.title.toLowerCase().includes(searchTerm)
+    );
+
+    setSearchResults(results);
+  }, [searchTerm]);
+
   return (
     <BrowserRouter>
       <div className="App">
-        <NavBar />
+        <NavBar
+          component={NavBar}
+          handleChange={handleChange}
+          searchTerm={searchTerm}
+        />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/">
+            <Home searchResults={searchResults} searchTerm={searchTerm} />
+          </Route>
           <Route path="/cart" component={Cart} />
           <Route path="/favorite" component={Fave} />
         </Switch>
@@ -22,4 +45,10 @@ function App() {
   );
 }
 
-export default App; 
+const mapStateToProps = (state: any) => {
+  return {
+    items: state.items,
+  };
+};
+
+export default connect(mapStateToProps)(App);
